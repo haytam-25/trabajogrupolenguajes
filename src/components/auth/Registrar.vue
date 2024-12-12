b343251c11f7670876165f3fa80723cedd6eef5f
ILias_Aharrouch <waelaran47@gmail.com>
2024-12-12 13:56:58 +0100

    cambios echos

--- src/components/auth/Registrar.vue
+++ src/components/auth/Registrar.vue
@@ -3,10 +3,13 @@
 </script>
 
 <template>
+  
 
 
 </template>
 
 <style scoped>
+
+
 
 </style>
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC1-ws7wf_jQBC4pLMv5hzt0_1sUjJV1AU",
+  authDomain: "proyectohaytamfirebase-d9b90.firebaseapp.com",
+  projectId: "proyectohaytamfirebase-d9b90",
+  storageBucket: "proyectohaytamfirebase-d9b90.firebasestorage.app",
+  messagingSenderId: "492743812356",
+  appId: "1:492743812356:web:76ad06b8cec9d0291c1c51"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
